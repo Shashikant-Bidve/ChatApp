aa1d29c51bef59c7a26dbdf3a8422675dd10e777
Shashikant Bidve <shashi.bidve18@gmail.com>
2024-04-22 16:49:38 +0530

    Deployment link provided

--- frontend/src/components/Miscelleneous/SingleChat.jsx
+++ frontend/src/components/Miscelleneous/SingleChat.jsx
@@ -12,7 +12,7 @@ import io from "socket.io-client";
 import Lottie, {} from "react-lottie";
 import animationData from "../../Animation - 1713558708658.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://chatapp-mklf.onrender.com";
 let socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
